2a02bcd6a5882d11dd728902fe6a638daa986285
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-12-07 19:10:57 +0200

    add basic

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 require("dotenv").config();
 const express = require("express");
 const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Jobs api");
+});
+
 app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
