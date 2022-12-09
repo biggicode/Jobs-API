c6df03cc1ef6e36f5d03758b0ed3cad6675e815c
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-12-09 18:59:59 +0200

    connect to db

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ require("express-async-errors");
 const express = require("express");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
-
 const app = express();
 
-//routes
+//connect DB
+const connectDB = require("./db/connect");
+
+//routers
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
 
 app.use(express.json());
-
+//routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", jobsRouter);
 
+//error handler and not found
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
@@ -22,6 +25,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () => {
       console.log(`Server is listening on port ${port}`);
     });
