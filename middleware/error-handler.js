3d6feac658f9bc5f499481a67d8c351ff301a9ca
Oncescu Laurentiu <larioncescu@yahoo.com>
2023-01-01 14:58:32 +0200

    validation error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,4 @@
 const { StatusCodes } = require("http-status-codes");
-const { CustomAPIError } = require("../errors");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
@@ -11,6 +10,12 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   // if (err instanceof CustomAPIError) {
   //   return res.status(err.statusCode).json({ msg: err.message });
   // }
+  if (err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+    customError.statusCode = 400;
+  }
 
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(
